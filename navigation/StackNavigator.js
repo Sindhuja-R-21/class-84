f30da31ebcd19dfb541b8dff4ef9465d87e8ba46
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2021-12-30 15:58:17 +0530

    Add files via upload

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigator from "./TabNavigator";
+import StoryScreen from "../screens/StoryScreen";
+
+const Stack = createStackNavigator();
+
